704750846ff6d791b558791710b9deeed255d3d9
mohsin6862 <mohsinctg2206@gmail.com>
2023-04-29 13:34:44 +0600

    News catagory created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors =require('cors')
 
 const catagories = require('./data/catagories.json');
+const news = require('./data/news.json')
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('My News paper is comming');
@@ -11,6 +12,29 @@ app.get('/',(req,res)=>{
 
 app.get('/catagories',(req,res)=>{
    res.send(catagories)
+})
+
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id == id)
+    res.send(selectedNews)
+})
+
+app.get('/catagories/:id',(req,res)=>{
+    const id = req.params.id;
+
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+
+        const singleCatagory = news.filter(n=> n.category_id == id)
+        res.send(singleCatagory)
+    }
 })
 app.listen(port,()=>{
     console.log(`News paper is running on ${port}`)
